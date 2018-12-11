be9e2f6f26cca91a36aa7ee4b2bf21805054691f
Yessoufou Samad <yessouf2009@outlook.com>
2018-12-11 18:15:36 +0000

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DatingApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    // http://localhost:500/api/values
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _ctx;
+
+        public ValuesController(DataContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var obj = await _ctx.Values.ToListAsync();
+
+            return Ok(obj);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var obj = await _ctx.Values.FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(obj);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=DatingApp.db");
+        }
+    }
+}
